e3a6a4f696a0c36877c0e53e127b071d8485cb01
simeoni <simeoni@simeoni-HP-14-Notebook-PC>
2017-12-11 21:41:38 +0100

    CMAKE presque fonctionnel

---
+++ build/CMakeFiles/alpha-mario.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/simeoni/Documents/ProjetJeuSDL/src/fonctionsBasiques.c" "/home/simeoni/Documents/ProjetJeuSDL/build/CMakeFiles/alpha-mario.dir/src/fonctionsBasiques.c.o"
+  "/home/simeoni/Documents/ProjetJeuSDL/src/fonctionsMenu.c" "/home/simeoni/Documents/ProjetJeuSDL/build/CMakeFiles/alpha-mario.dir/src/fonctionsMenu.c.o"
+  "/home/simeoni/Documents/ProjetJeuSDL/src/fonctionsPersonnage.c" "/home/simeoni/Documents/ProjetJeuSDL/build/CMakeFiles/alpha-mario.dir/src/fonctionsPersonnage.c.o"
+  "/home/simeoni/Documents/ProjetJeuSDL/src/main.c" "/home/simeoni/Documents/ProjetJeuSDL/build/CMakeFiles/alpha-mario.dir/src/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../alpha-mario"
+  "/usr/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/alpha-mario.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/alpha-mario.dir/src/main.c.o"
+  "CMakeFiles/alpha-mario.dir/src/fonctionsBasiques.c.o"
+  "CMakeFiles/alpha-mario.dir/src/fonctionsMenu.c.o"
+  "CMakeFiles/alpha-mario.dir/src/fonctionsPersonnage.c.o"
+  "alpha-mario.pdb"
+  "alpha-mario"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/alpha-mario.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
